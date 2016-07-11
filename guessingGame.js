585fcb5faba624bdf24223d170d2ba114c6b0731
Ryan Driscoll <driscollrp@gmail.com>
2016-07-11 10:43:29 -0500

    new changes, late submission

--- guessingGame.js
+++ guessingGame.js
@@ -1,8 +1,17 @@
 /* **** Global Variables **** */
 // try to elminate these global variables in your project, these are here just to start.
 
-var playersGuess,
-    winningNumber
+
+
+var winningNumber = generateWinningNumber();
+
+
+$(document).ready(function() {
+	$('#submit').click(function() {
+		playersGuessSubmission();
+	});
+});
+  
 
 
 
@@ -11,25 +20,29 @@ var playersGuess,
 // Generate the Winning Number
 
 function generateWinningNumber(){
-	// add code here
-}
+	return Math.floor(Math.random()*100) + 1;
+};
+
 
 // Fetch the Players Guess
 
 function playersGuessSubmission(){
+	var playersGuess = $("input[name=number]").val();
+
 	// add code here
 }
 
 // Determine if the next guess should be a lower or higher number
 
 function lowerOrHigher(){
-	// add code here
 }
 
 // Check if the Player's Guess is the winning number 
 
 function checkGuess(){
-	// add code here
+	if (playersGuess === winningNumber) {
+
+	}
 }
 
 // Create a provide hint button that provides additional clues to the "Player"
@@ -44,6 +57,16 @@ function playAgain(){
 	// add code here
 }
 
-
 /* **** Event Listeners/Handlers ****  */
 
+$(document).ready(function() {
+    
+    $('button').hover(
+    function() {
+        $(this).addClass('highlight');
+    },
+    function() {
+    	$(this).removeClass('highlight');
+    })
+
+});  
